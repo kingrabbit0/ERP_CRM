1c8347561062f73bb91c29c0b9d8a895189fadcf
kingrabbit0 <bohdanlutsenko760@gmail.com>
2025-01-22 03:00:14 -0500

    [feat] update the server port to 8888

--- backend/app.js
+++ backend/app.js
@@ -23,13 +23,14 @@ const notificationController = require('@/controllers/appControllers/notificatio
 // create our Express app
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('ngrok-skip-browser-warning', '69420');
+//   next();
+// });
 
 const corsOptions = {
   origin: true,
--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ glob.sync('./models/**/*.js').forEach(function (file) {
 
 // Start our app!
 const app = require('./app');
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 8888);
 
 // const server = app.listen(app.get('port'), () => {
 //   console.log(`Express running → On PORT : ${server.address().port}`);
