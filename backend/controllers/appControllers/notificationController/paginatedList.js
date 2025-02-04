061fa7cc6cc36ef4d3c3f54a597c7643ddeee6d7
kingrabbit0 <bohdanlutsenko760@gmail.com>
2025-02-04 14:28:44 -0500

    [feat] create notification for the equipments that is created before

--- backend/controllers/appControllers/notificationController/paginatedList.js
+++ backend/controllers/appControllers/notificationController/paginatedList.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
 
 const Model = mongoose.model('Notification');
+const EquipmentModel = mongoose.model('Equipment');
+
+const CreateController = require('./create');
 
 const paginatedList = async (req, res) => {
   // const page = req.query.page || 1;
   // const limit = parseInt(req.query.items) || 10;
   // const skip = page * limit - limit;
   try {
+    const equipments = await EquipmentModel.find({ removed: false });
+    for (let i = 0; i < equipments.length; i++) {
+      const isExist = await Model.find({equipment: equipments[i]._id});
+      if (isExist.length == 0) 
+        await CreateController(equipments[i]);
+    }
+
     //  Query the database for a list of all results
     const resultsPromise = Model.find({ removed: false })
       .sort({ created: 'desc' })
